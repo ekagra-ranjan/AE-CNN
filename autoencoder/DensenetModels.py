5230c4da39a00c5a55b6bbf0336bea4748294fe6
ekka <ekagra.ranjan@gmail.com>
2018-11-04 02:36:22 +0530

    Update DensenetModels.py

--- autoencoder/DensenetModels.py
+++ autoencoder/DensenetModels.py
@@ -21,37 +21,21 @@ class Autoencoder(nn.Module):
 
         self.encoder = nn.Sequential(
             #1x896x896
-            nn.Conv2d(in_channels = 1, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
-            nn.LeakyReLU(),
-            #64x896x896
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            #64x448x448
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            #64x448x448
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            #64x224x224
-            nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU()
-            #3x224x224
+            nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5, stride = 4, padding = 2),
+            nn.ELU(),
+            #1X224X224
+            nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0),
+            #1x224x224
             )
 
         self.decoder = nn.Sequential(
-            #3x224x224
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(),
-            #64x224x224
-            nn.UpsamplingNearest2d(scale_factor=2),
-            #64x448x448
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(),
-            #64x448x448
-            nn.UpsamplingNearest2d(scale_factor=2),
-            #64x896x896
-            nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1),
+            #1x224x224
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.PixelShuffle(4),
             #1x896x896
-            #nn.Sigmoid()
             )
+        
+        
 
     def forward(self, x):
         
@@ -76,8 +60,8 @@ class Relu1(Function):
 
         input, = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad_input[input<0]*=0.01
-        grad_input[input>1]*=0.01
+        grad_input[input<0]*=0.0
+        grad_input[input>1]*=0.0
 
         return grad_input
 
